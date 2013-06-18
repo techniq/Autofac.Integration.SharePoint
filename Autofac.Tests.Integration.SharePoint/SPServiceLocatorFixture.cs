62ad3b45be0338cebec5d503e0324511c61d88cf
Sean Lynch <sean.lynch@sbcs.com>
2013-06-18 14:52:53 -0400

    Exclude missing WebApp_DemoFeature from Demo project and comment out unimplemented tests

--- Autofac.Tests.Integration.SharePoint/SPServiceLocatorFixture.cs
+++ Autofac.Tests.Integration.SharePoint/SPServiceLocatorFixture.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -64,3 +65,4 @@ namespace Autofac.Tests.Integration.SharePoint
         }
     }
 }
+*/
